ce5edbbbe4f6bcc6ae1b8f8ed6e28dd31df6040c
123 <1400208597@qq.com>
2020-10-29 19:33:41 +0800

    add Math_arithmetic_node, Logic_compare_node

--- src/main/java/core/Logic_compare_node.java
+++ src/main/java/core/Logic_compare_node.java
@@ -1,19 +1,123 @@
 package core;
 
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import parserTool.Parser;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Logic_compare_node implements Block_node {
-    public static Logic_compare_node logic_compare_node_factory(Node node){
-        return new Logic_compare_node();
+    private Block_node val_A;
+    private Block_node val_B;
+    private String op;
+
+    private Logic_compare_node(Block_node val_A, Block_node val_B, String op) {
+        this.val_A = val_A;
+        this.val_B = val_B;
+        this.op = op;
+    }
+
+    public static Logic_compare_node logic_compare_node_factory(Node node) {
+        Set<String> validTypeSet = new HashSet<>();
+        validTypeSet.add("math_number");
+        validTypeSet.add("math_arithmetic");
+        validTypeSet.add("logic_compare");
+        validTypeSet.add("text");
+
+        NodeList nodeList = node.getChildNodes();
+        int nodeNum = nodeList.getLength();
+
+        String op = nodeList.item(1).getTextContent(); // 待解析运算符
+
+        if (nodeNum == 2) { // 只有运算符
+            return new Logic_compare_node(null, null, op);
+        } else if (nodeNum == 4) { // 有一个操作数
+            Node currChild = nodeList.item(3);
+            String type = currChild.getAttributes().getNamedItem("name").getTextContent();
+            if (type.equals("A")) { // 左操作数
+                return new Logic_compare_node(Parser.parseNodeToBlockNode(currChild.getChildNodes().item(1), validTypeSet), null, op);
+            } else { // 右操作数
+                return new Logic_compare_node(null, Parser.parseNodeToBlockNode(currChild.getChildNodes().item(1), validTypeSet), op);
+            }
+        } else { // 两个操作数
+            Node val_A = nodeList.item(3).getChildNodes().item(1);
+            Node val_B = nodeList.item(5).getChildNodes().item(1);
+            return new Logic_compare_node(Parser.parseNodeToBlockNode(val_A, validTypeSet), Parser.parseNodeToBlockNode(val_B, validTypeSet), op);
+        }
     }
 
     @Override
     public String toJavaCode() {
-        return null;
+        String opToJava = "";
+        String a = " ";
+        String b = " ";
+
+        if (val_A != null) {
+            a = val_A.toJavaCode();
+        }
+        if (val_B != null) {
+            b = val_B.toJavaCode();
+        }
+
+        boolean aIsString = val_A instanceof Text_node;
+        boolean bIsString = val_B instanceof Text_node;
+
+        switch (op) {
+            case "EQ":
+                if (aIsString && bIsString) {
+                    opToJava = a + ".equals(" + b + ")";
+                } else {
+                    opToJava = "(" + a + "==" + b + ")";
+                }
+                break;
+            case "NEQ":
+                if (aIsString && bIsString) {
+                    opToJava = "(!" + a + ".equals(" + b + "))";
+                } else {
+                    opToJava = "(" + a + "!=" + b + ")";
+                }
+                break;
+            case "LT":
+                if (aIsString && bIsString) {
+                    opToJava = "(" + a + ".compareTo(" + b + ") < 0)";
+                } else {
+                    opToJava = "(" + a + "<" + b + ")";
+                }
+                break;
+            case "LTE":
+                if (aIsString && bIsString) {
+                    opToJava = "(" + a + ".compareTo(" + b + ") <= 0)";
+                } else {
+                    opToJava = "(" + a + "<=" + b + ")";
+                }
+                break;
+            case "GT":
+                if (aIsString && bIsString) {
+                    opToJava = "(" + a + ".compareTo(" + b + ") > 0)";
+                } else {
+                    opToJava = "(" + a + ">" + b + ")";
+                }
+                break;
+            case "GTE":
+                if (aIsString && bIsString) {
+                    opToJava = "(" + a + ".compareTo(" + b + ") >= 0)";
+                } else {
+                    opToJava = "(" + a + ">=" + b + ")";
+                }
+                break;
+        }
+        return opToJava;
     }
 
     @Override
     public String toXML() {
-        return null;
+        String a = val_A.toXML();
+        String b = val_B.toXML();
+        return "<block type=\"logic_compare\" id=\"\" x=\"\" y=\"\">" +
+                "<field name=\"OP\">" + op + "</field>" +
+                "<value name=\"A\">" + a + "</value>" +
+                "<value name=\"B\">" + b + "</value>" +
+                "</block>";
     }
 }
